6dadaaf34986874b0e6b4c1e91d240349278bd65
luanosdev <luan.o.s.dev@gmail.com>
2025-06-22 15:56:47 -0300

    Adiciona suporte para drops garantidos de MVP no DropManager, implementando a lógica para processar e selecionar itens aleatórios a partir de uma nova tabela de drops de MVP. Mantém a documentação atualizada.

---
+++ src/data/mvp_drops.lua
@@ -0,0 +1,17 @@
+---@class MvpDrop
+---@field itemId string
+
+---@type MvpDrop[]
+local mvp_drops = {
+    {
+        itemId = "rune_orbital_e",
+    },
+    {
+        itemId = "rune_thunder_e",
+    },
+    {
+        itemId = "rune_aura_e",
+    },
+}
+
+return mvp_drops
